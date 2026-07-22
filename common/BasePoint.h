f99b6e6ecfd8b2e68ee014ae20d8ca80170a06ad
torch-5080 <1983561291@qq.com>
2026-07-22 05:18:10 +0000

    Add BasePoint class and corresponding unit tests; configure CMake for testing

---
+++ common/BasePoint.h
@@ -0,0 +1,48 @@
+#ifndef COMMON_BASE_POINT_H
+#define COMMON_BASE_POINT_H
+
+#include <type_traits>
+
+namespace common {
+
+template <typename point_type,
+          std::enable_if_t<std::is_arithmetic_v<point_type>, int> = 0>
+class BasePoint {
+public:
+    constexpr BasePoint() = default;
+    constexpr BasePoint(point_type x, point_type y)
+        : m_x(x), m_y(y) {}
+    ~BasePoint() = default;
+
+    constexpr point_type& x() {
+        return m_x;
+    }
+
+    constexpr const point_type& x() const {
+        return m_x;
+    }
+
+    constexpr point_type& y() {
+        return m_y;
+    }
+
+    constexpr const point_type& y() const {
+        return m_y;
+    }
+
+    constexpr void set_x(point_type x) {
+        m_x = x;
+    }
+
+    constexpr void set_y(point_type y) {
+        m_y = y;
+    }
+
+private:
+    point_type m_x = 0;
+    point_type m_y = 0;
+};
+
+} // namespace common
+
+#endif // COMMON_BASE_POINT_H
---
+++ common/unittest/BasePointTest.cpp
@@ -0,0 +1,68 @@
+#include "common/BasePoint.h"
+
+#include <gtest/gtest.h>
+
+#include <string>
+#include <type_traits>
+
+namespace {
+
+template <typename type, typename = void>
+struct is_point_coordinate_type : std::false_type {};
+
+template <typename type>
+struct is_point_coordinate_type<
+    type, std::void_t<decltype(sizeof(common::BasePoint<type>))>>
+    : std::true_type {};
+
+static_assert(is_point_coordinate_type<int>::value);
+static_assert(is_point_coordinate_type<double>::value);
+static_assert(!is_point_coordinate_type<std::string>::value);
+
+} // namespace
+
+namespace common::unittest {
+
+TEST(BasePointTest, DefaultConstructionInitializesCoordinatesToZero) {
+    common::BasePoint<int> origin;
+    EXPECT_EQ(origin.x(), 0);
+    EXPECT_EQ(origin.y(), 0);
+}
+
+TEST(BasePointTest, ValueConstructionSetsCoordinates) {
+    common::BasePoint<int> point{1, 2};
+    EXPECT_EQ(point.x(), 1);
+    EXPECT_EQ(point.y(), 2);
+}
+
+TEST(BasePointTest, SupportsFloatingPointCoordinates) {
+    common::BasePoint<double> point{1.5, 2.25};
+    EXPECT_DOUBLE_EQ(point.x(), 1.5);
+    EXPECT_DOUBLE_EQ(point.y(), 2.25);
+
+    point.set_x(3.75);
+    point.set_y(4.5);
+    EXPECT_DOUBLE_EQ(point.x(), 3.75);
+    EXPECT_DOUBLE_EQ(point.y(), 4.5);
+}
+
+TEST(BasePointTest, RejectsNonNumericCoordinateTypes) {
+    EXPECT_FALSE((is_point_coordinate_type<std::string>::value));
+}
+
+TEST(BasePointTest, CoordinatesCanBeMutated) {
+    common::BasePoint<int> point;
+    point.set_x(3);
+    point.y() = 4;
+    EXPECT_EQ(point.x(), 3);
+    EXPECT_EQ(point.y(), 4);
+}
+
+TEST(BasePointTest, ConstAccessorsReadCoordinates) {
+    common::BasePoint<int> point{3, 4};
+    const common::BasePoint<int>& constant_point = point;
+    EXPECT_EQ(constant_point.x(), 3);
+    EXPECT_EQ(constant_point.y(), 4);
+}
+
+} // namespace common::unittest
